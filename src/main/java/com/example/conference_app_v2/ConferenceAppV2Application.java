ba28c697437152e783724f3bbd1f531ab50df7b6
Michael Male <malmike21@gmail.com>
2020-09-30 11:36:15 +0300

    initial commit

---
+++ src/main/java/com/example/conference_app_v2/ConferenceAppV2Application.java
@@ -0,0 +1,13 @@
+package com.example.conference_app_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/conference_app_v2/ConferenceAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conference_app_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
